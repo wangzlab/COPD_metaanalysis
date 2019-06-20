3297e78e5201635338a3f834a6c19caace5293f1
wangzlab <51474812+wangzlab@users.noreply.github.com>
2019-06-20 17:56:53 +0800

    Create 8_generate_EMM_for_PRMT.pl

---
+++ 8_generate_EMM_for_PRMT.pl
@@ -0,0 +1,83 @@
+open (IN, $ARGV[0]); 
+my %contrib=();
+#### metabolic reactions with info as: EC\tLeftcmpds\tRightcmpds\tReactiondirection\n as adapted from MetaCyc database ######
+while (<IN>) {
+	chop;
+	@a=split("\t",$_);
+	@left=split(";",$a[1]);
+	@right=split(";",$a[2]);
+	if ($a[3]=~ /LEFT-TO-RIGHT/) {
+		for my $val (@left) {
+			$contrib{$a[0]}{$val}--;
+			$revcon{$val}{$a[0]}--;
+		}
+		for my $val2 (@right) {
+			$contrib{$a[0]}{$val2}++;
+			$revcon{$val2}{$a[0]}++;
+		}
+	}
+	elsif ($a[3]=~ /RIGHT-TO-LEFT/) {
+		for my $val (@left) {
+			$contrib{$a[0]}{$val}++;
+			$revcon{$val}{$a[0]}++;
+		}
+		for my $val2 (@right) {
+			$contrib{$a[0]}{$val2}--;
+			$revcon{$val2}{$a[0]}--;
+		}
+	}
+	elsif ($a[3] eq '') {
+		for my $val (@left) {
+			$contrib{$a[0]}{$val}--;
+			$revcon{$val}{$a[0]}--;
+		}
+		for my $val2 (@right) {
+			$contrib{$a[0]}{$val2}++;
+			$revcon{$val2}{$a[0]}++;
+		}
+	}
+}
+print "CMPD";
+for my $key (sort keys %contrib) {
+	print "\t$key";
+}
+print "\n";
+
+for my $key (sort keys %revcon) {
+	for my $key2 (sort keys %contrib) {
+		if (! exists $revcon{$key}{$key2}) {
+			$revcon{$key}{$key2}=0;
+		}
+	}	
+}
+
+for my $key (sort keys %revcon) {
+	my $pos=0;
+	my $neg=0;
+	for my $key2 (sort keys %{$revcon{$key}}) {
+		if ($revcon{$key}{$key2} > 0) {
+			$pos += $revcon{$key}{$key2};
+		}
+		elsif ($revcon{$key}{$key2} < 0) {
+			$neg+=abs($revcon{$key}{$key2});
+		}
+	}
+	for my $key2 (sort keys %{$revcon{$key}}) {
+		if ($revcon{$key}{$key2} > 0) {
+			$normrev{$key}{$key2}=$revcon{$key}{$key2}/$pos;
+		}
+		elsif($revcon{$key}{$key2}<0) {
+			$normrev{$key}{$key2}=$revcon{$key}{$key2}/$neg;
+		}
+		else {
+			$normrev{$key}{$key2}=0;
+		}
+	}
+}
+for my $key (sort keys %normrev) {
+	print $key;
+	for my $key2 (sort keys %{$normrev{$key}}) {
+		print "\t$normrev{$key}{$key2}";
+	}
+	print "\n";
+}
