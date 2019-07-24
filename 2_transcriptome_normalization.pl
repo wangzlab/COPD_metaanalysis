97867c1e5f8ecfad66f5b85417899ef2a1f2dff6
wangzlab <51474812+wangzlab@users.noreply.github.com>
2019-07-24 16:28:48 +0800

    Update 2_transcriptome_normalization.pl

--- 2_transcriptome_normalization.pl
+++ 2_transcriptome_normalization.pl
@@ -1,13 +1,18 @@
 #!bin/perl
+## This script is used to perform log2 transformation and z-score normalization for all processed microarray data ##
+## input is a list of datasets to be processed and the list of datasets that need to be log2 transformed ##
+
+use lib "/home/wangzhang/perl5/lib/perl5/";
 use Statistics::Descriptive;
-open (IN, "log2_transform_list"); 
+
+open (IN, "logtrans_data_list"); 
 ### list of data that need to be log transformed ####
 ### some data are already in log normal distribution therefore log2 transform no longer needed #####
 while (<IN>) {
 	chop;
 	$logtran{$_}=1;
 }
-open (IN, $ARGV[0]); ### input dataset list ####
+open (IN, "dataset_list"); ### input dataset list ####
 while (<IN>) {
 	chop;
 	$id=$_;
