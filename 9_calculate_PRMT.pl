b4ba85c355984da05da7c3efc3ca8948e82ac67a
wangzlab <51474812+wangzlab@users.noreply.github.com>
2019-07-24 17:17:38 +0800

    Update 9_calculate_PRMT.pl

--- 9_calculate_PRMT.pl
+++ 9_calculate_PRMT.pl
@@ -1,5 +1,9 @@
-open (IN, $ARGV[0]); 
-#### a two column table for the combined effect size of each microbial gene ##########
+#!bin/perl
+### This is the script to generate the PRMT score of the metabolites ###
+### based on the combined fold change of microbial genes in the meta-analysis and the EMM matrix generated in the previous step ###
+
+open (IN, "metaanalysis_microbial_genes.txt"); 
+### This is a two column table for the microbial gene and its combined effect size in the meta-analysis ###
 while (<IN>) {
 	chop;
 	@a=split("\t",$_);
@@ -7,6 +11,7 @@ while (<IN>) {
 }
 
 open (IN, "EMM.txt");
+### This is the EMM matrix generated in the previous step ###
 $header=<IN>;
 chop $header;
 @headers=split("\t",$header);
